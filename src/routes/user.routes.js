69410d850c953f0182b0732fd8caa14949b83a04
Tohir Kuliev <kuliev.tech@gmail.com>
2023-11-07 11:38:13 -0500

    handle routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { userController } from "../controllers/user.controller.js";
+import { userMiddleware } from "../middlewares/user.middleware.js";
+
+const userRouter = Router();
+
+userRouter.post("/sign-up", userController.signUp);
+userRouter.post("/login", userController.login);
+userRouter.patch("/forgot-password", userController.forgotPassword);
+userRouter.patch("/reset-password", userController.resetPassword);
+userRouter.get("/me", userMiddleware.authenticate, userController.getMe);
+userRouter.delete(
+    "/logout",
+    userMiddleware.authenticate,
+    userController.logout
+);
+
+export { userRouter };
